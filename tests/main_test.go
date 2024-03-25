12c87db2b6e584b865104f337751fc938ad8d745
Aaditya <athakur4_be22@thapar.edu>
2024-03-25 18:38:21 +0530

    add tests

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,1 +1,49 @@
 package tests
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestByteCount(t *testing.T) {
+	cmd := exec.Command("../build", "-c", "../test.txt")
+
+	out, err := cmd.Output()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	s := string(out)
+	bef, _, _ := strings.Cut(s, " ")
+	if bef != "335001" {
+		t.Fail()
+	}
+}
+
+func TestLineCount(t *testing.T) {
+	cmd := exec.Command("../build", "-l", "../test.txt")
+
+	out, err := cmd.Output()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	s := string(out)
+	bef, _, _ := strings.Cut(s, " ")
+	if bef != "7147" {
+		t.Fail()
+	}
+}
+
+func TestWordCount(t *testing.T) {
+	cmd := exec.Command("../build", "-w", "../test.txt")
+
+	out, err := cmd.Output()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	s := string(out)
+	bef, _, _ := strings.Cut(s, " ")
+	if bef != "58070" {
+		t.Fail()
+	}
+}
